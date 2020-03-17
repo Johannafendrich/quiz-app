26ff074bbdf33f29dd4c942315d85e3606c6fb84
Johannafendrich <fendrichjohanna@gmail.com>
2020-03-17 08:00:39 +0100

    Change styling from pages with styled components

---
+++ src/components/IntroSection.js
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+const IntroSection = styled.section`
+  width: 60%;
+  height: 80px;
+  margin-top: 20px;
+  margin-bottom: 150px;
+  padding: 20px;
+  color: #333333;
+`;
+
+export default IntroSection;
--- src/pages/Result.js
+++ src/pages/Result.js
@@ -3,15 +3,14 @@ import { Link } from 'react-router-dom';
 import AppHeader from '../components/AppHeader';
 import Card from '../components/Card';
 import './Result.css';
+import Title from '../components/Title';
 
 function Result() {
   return (
     <>
       <AppHeader />
       <Card>
-        <div className="quiz">
-          <h2 className="result">Glückwunsch, du bist ein echter Jeck!</h2>
-        </div>
+        <Title>Glückwunsch, du bist ein echter Jeck!</Title>
         <img
           src="https://images.unsplash.com/photo-1561624485-0e43bcc1836d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80"
           alt="Cologne"
